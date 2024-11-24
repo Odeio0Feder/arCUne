90f74c2ebb38fa2b3c6f8476a4253bdc2717226c
VTMNC feber <168595872+Odeio0Feder@users.noreply.github.com>
2024-11-24 17:43:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Programação',
+    'Heimerdinger',
+    'Morto'
+)
+
+criaCartao(
+    'Geografia',
+    'Vander (Warwick)',
+    '"Morto"'
+)
+
+criaCartao(
+    'Programação',
+    'Vi',
+    'Viva (curtindo a vida lésbica)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Jayce',
+    'Morto'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Viktor',
+    'Morto'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Jinx',
+    '"Morta"'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Caitlyn',
+    'Viva (curtindo a vida lésbica)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Ekko',
+    'Vivo (todo fudid0)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Singed',
+    'Vivo (final feliz)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Ambessa',
+    'Morta (final feliz)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Mel',
+    'Viva'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Sevika',
+    'Viva'
+)
